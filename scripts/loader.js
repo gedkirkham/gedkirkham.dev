345254f8c9d24f32cfcee5b9dc93edaf8ca0f95a
Ged Kirkham <gedkirkham@protonmail.com>
2020-04-01 15:25:28 +0100

    add files

---
+++ scripts/loader.js
@@ -0,0 +1,42 @@
+console.log('loader.js')
+
+window.loadComponent = (function() {
+    console.log('loadComponent')
+    function loadComponentAA(URL) {
+        console.log('inside loadComponent')
+        // const parser = new DOMParser()
+    //     let stringitem = `<div id="ribbon-wrapper">
+    //     <div id="ribbon"></div>
+    // </div>
+    
+    // <header id="grid-header">
+    //     <h1><a href="/">Ged Kirkhamaaaaaaaaaaa</a></h1>
+    // </header>
+    
+    // <style>
+    
+    // </style>
+    
+    // <script>
+    
+    // </script>`
+        // return parser.parseFromString(URL, 'text/html')
+
+        return fetch( URL ).then( ( response ) => {
+            console.log(response.text())
+            return response.text();
+          } )
+          .then( ( html ) => {
+            const parser = new DOMParser(); // 1
+          
+            return parser.parseFromString( html, 'text/html' ); // 2
+          } );
+    }
+    console.log('before return loadComponet')
+    return loadComponentAA
+}())
+
+
+// fetch( './components/header.wc' ).then( ( response ) => {
+//     return response.text();
+//   } )
