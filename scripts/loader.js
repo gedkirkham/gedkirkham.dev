97daa27a571115229f97950a24bfb153ff73dbcf
Ged Kirkham <gedkirkham@protonmail.com>
2020-04-01 16:37:21 +0100

    tidy

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,46 +1,42 @@
-console.log('loader.js')
-
 window.loadComponent = (function() {
-    console.log('loadComponent')
     function fetchAndParse(URL) {
-        return fetch( URL ).then( ( response ) => {
-            return response.text();
-          } ).then( ( html ) => {
-            const parser = new DOMParser();
-            const document = parser.parseFromString( html, 'text/html' );
-            const head = document.head;
-            const template = head.querySelector( 'template' );
-            const style = head.querySelector( 'style' );
-            const script = head.querySelector( 'script' );
+        return fetch( URL )
+            .then(response => { return response.text() })
+            .then(html => {
+                const parser = new DOMParser()
+                const document = parser.parseFromString( html, 'text/html' )
+                const head = document.head
+                const template = head.querySelector( 'template' )
+                const style = head.querySelector( 'style' )
+                const script = head.querySelector( 'script' )
           
-            return {
-              template,
-              style,
-              script
-            };
-          } );
+                return {
+                    template,
+                    style,
+                    script
+                }
+            })
         }
     
-        function registerComponent( { template, style, script } ) {
-            class UnityComponent extends HTMLElement {
-              connectedCallback() {
-                this._upcast();
-              }
-          
-              _upcast() {
-                const shadow = this.attachShadow( { mode: 'open' } );
-          
-                shadow.appendChild( style.cloneNode( true ) );
-                shadow.appendChild( document.importNode( template.content, true ) );
-              }
+    function registerComponent({ template, style, script }) {
+        class UnityComponent extends HTMLElement {
+            connectedCallback() {
+                this._upcast()
             }
-            return customElements.define( 'g-header', UnityComponent );
-          }
+        
+            _upcast() {
+                const shadow = this.attachShadow( { mode: 'open' } )
+            
+                shadow.appendChild( style.cloneNode( true ) )
+                shadow.appendChild( document.importNode( template.content, true ) )
+            }
+        }
+        return customElements.define( 'g-header', UnityComponent )
+    }
     
     function loadComponent(URL) {
-        console.log('inside loadComponent')
-        console.log('before return loadComponet')
         return fetchAndParse(URL).then(registerComponent)
     }
+
     return loadComponent
 }())
