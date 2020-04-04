95782c90bc15f966009f1fd1bded48eb23b37acb
Ged Kirkham <gedkirkham@protonmail.com>
2020-04-04 17:54:00 +0100

    refactor: update CACHE_NAME to v6

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@ const FILES_TO_CACHE = [
     '/blog/post/loading-icon-button-tutorial.html'
 ]
 
-const CACHE_NAME = 'static-cache-v5'
+const CACHE_NAME = 'static-cache-v6'
 
 self.addEventListener('install', event => {
     event.waitUntil(
