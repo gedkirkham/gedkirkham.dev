3bdcb2221ee9cba31f37d19a2283e08b8a68b18f
ged <gedkirkham@protonmail.com>
2021-02-16 19:53:01 +0000

    refactor: update service worker

--- service-worker.js
+++ service-worker.js
@@ -13,7 +13,7 @@ const FILES_TO_CACHE = [
     '/scripts/loader.js'
 ]
 
-const CACHE_NAME = 'static-cache-v10'
+const CACHE_NAME = 'static-cache-v11'
 
 self.addEventListener('install', event => {
     event.waitUntil(
