d1fc78970292a3c2c1f2f5e41d4a87be4f6b6cb4
ged <gedkirkham@protonmail.com>
2021-04-01 20:03:51 +0100

    refactor: update service worker cache value

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ const FILES_TO_CACHE = [
     '/node_modules/gk-wc-button/button.wc',
 ]
 
-const CACHE_NAME = 'static-cache-v16'
+const CACHE_NAME = 'static-cache-v17'
 
 self.addEventListener('install', event => {
     event.waitUntil(
