5874979ebe98fb687d559ae291907279a291a04a
Ged Kirkham <gedkirkham@protonmail.com>
2023-12-13 09:18:35 +0000

    Update service-worker-v2.js

--- service-worker-v2.js
+++ service-worker-v2.js
@@ -14,7 +14,7 @@ const FILES_TO_CACHE = [
     '/node_modules/gk-wc-button/button.wc',
 ]
 
-const CACHE_NAME = 'static-cache-v19'
+const CACHE_NAME = 'static-cache-v20'
 
 self.addEventListener('install', event => {
     event.waitUntil(
